f002678d5adc33fe4c2b659a8f1992b93a550500
Sarvesh Kumar <sarveshkumar10101@gmail.com>
2024-04-12 23:26:01 +0530

    update

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Check if the request contains a file
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
 
-@app.route('/hello')
-def Yellow():
-    return "Yellow!"
+    file = request.files['file']
+
+    # Check if the file is empty
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    # Assuming it's an image file, you can save it to a directory
+    file.save('uploads/' + file.filename)
+
+    return jsonify({'message': 'File successfully uploaded'})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
